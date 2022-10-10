147586aee35ca7960a4f4e1d1abf4643634a5150
vino <vino0908@outlook.com>
2022-10-10 21:34:49 +0800

    fix: 使用 before_create 解决验证码不一致的问题

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
-    validation_code = ValidationCode.new email: params[:email], kind: "sign_in" 
+    validation_code = ValidationCode.new email: params[:email], kind: "sign_up"
     if validation_code.save
       render json: { message: "验证码已发送" }, status: 201
     else
--- app/models/validation_code.rb
+++ app/models/validation_code.rb
@@ -1,7 +1,8 @@
 class ValidationCode < ApplicationRecord
   validates :email, presence: true
-  after_initialize :generate_code
+  before_create :generate_code
   after_create :send_validation_code
+  enum kind: { sign_in: 0, sign_up: 1 }
 
   def generate_code
     self.code = SecureRandom.random_number.to_s[2..7]
